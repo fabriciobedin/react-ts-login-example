92c352530140a7d66d91d473f17a4a6cea0153e5
Fabricio Bedin <fabricioobedin@gmail.com>
2020-08-12 22:02:39 -0300

    adding input validations

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,39 +1,41 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
 import { Container, Content, Background } from './styles';
+import getValidationErrors from '../../utils/getValidationErrors';
 import logo from '../../assets/logo_upf.png';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório!'),
         email: Yup.string()
-          .required('Email obrigatório')
+          .required('Email obrigatório!')
           .email('Digite um email válido!'),
-        passwork: Yup.string().min(
-          6,
-          'A Senha deve conter no mínimo 6 dígitos!'
-        )
+        password: Yup.string().min(6, 'Mínimo 6 dígitos!')
       });
       await schema.validate(data, {
         abortEarly: false
       });
     } catch (err) {
-      console.log(err);
+      formRef.current?.setErrors(getValidationErrors(err));
     }
   }, []);
   return (
     <Container>
       <Content>
         <img src={logo} alt="UPF" />
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
           <Input name="name" icon={FiUser} placeholder="Nome Completo" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
